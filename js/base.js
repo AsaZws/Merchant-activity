bafbaab7232a29c0b72762d85a863c803042bf84
AsaZws <zhouws.com@gmail.com>
2019-01-11 12:24:27 +0800

    大的流程

--- js/base.js
+++ js/base.js
@@ -5,47 +5,19 @@
  * 
  */
 
-(function flexible (window, document) {
-  var docEl = document.documentElement
-  var dpr = window.devicePixelRatio || 1
-
-  // adjust body font size
-  function setBodyFontSize () {
-    if (document.body) {
-      document.body.style.fontSize = (12 * dpr) + 'px'
-    }
-    else {
-      document.addEventListener('DOMContentLoaded', setBodyFontSize)
-    }
-  }
-  setBodyFontSize();
-
-  // set 1rem = viewWidth / 10
-  function setRemUnit () {
-    var rem = docEl.clientWidth / 10
-    docEl.style.fontSize = rem + 'px'
-  }
-
-  setRemUnit()
-
-  // reset rem unit on page resize
-  window.addEventListener('resize', setRemUnit)
-  window.addEventListener('pageshow', function (e) {
-    if (e.persisted) {
-      setRemUnit()
-    }
-  })
-
-  // detect 0.5px supports
-  if (dpr >= 2) {
-    var fakeBody = document.createElement('body')
-    var testElement = document.createElement('div')
-    testElement.style.border = '.5px solid transparent'
-    fakeBody.appendChild(testElement)
-    docEl.appendChild(fakeBody)
-    if (testElement.offsetHeight === 1) {
-      docEl.classList.add('hairlines')
-    }
-    docEl.removeChild(fakeBody)
-  }
-}(window, document))
+(function(doc, win) {
+  var html = doc.getElementsByTagName("html")[0],
+    // orientationchange->手机屏幕转屏事件
+    // resize->页面大小改变事件(一边pc端有用)
+    reEvt = "orientationchange" in win ? "orientationchange" : "resize",
+    reFontSize = function() {
+      var clientW = doc.documentElement.clientWidth || doc.body.clientWidth;
+      if (!clientW) {
+        return;
+      }
+      html.style.fontSize = 100 * (clientW / 375) + "px";
+    };
+  win.addEventListener(reEvt, reFontSize);
+  // DOMContentLoaded->dom加载完就执行,onload要dom/css/js都加载完才执行
+  doc.addEventListener("DOMContentLoaded", reFontSize);
+})(document, window);
